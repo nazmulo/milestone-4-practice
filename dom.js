cfb87cfd721bea5e21aa082a176165074b9a4e0a
nazmulo <itsnaazmul@gmail.com>
2026-08-26 15:04:24 +0600

    get elememt by tagname

--- dom.js
+++ dom.js
@@ -3,3 +3,15 @@
 //Traversing DOM :
 
 // document.getElementByTagName ('li');
+
+//1
+
+//const liCollection = document.getElementsByTagName('li');
+//
+//console.log(liCollection);
+//
+//for (const li of liCollection) {
+//    console.log(li);
+//}
+//
+//console.log(li.innerText);
