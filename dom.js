6548b962f07c702fd33fda90fc3c18685baed281
nazmulo <itsnaazmul@gmail.com>
2026-08-26 15:56:53 +0600

    get element by id

--- dom.js
+++ dom.js
@@ -30,3 +30,19 @@
 
 //const allHeading = document.getElementsByTagName (h1);
 //       allHeading.innerText = 'Jahan';
+
+//document.get elementById ;
+
+//1
+
+//const foodTittle = document.getElementById('food');
+//console.log ('foodTittle');
+//console.log ('foodTittle.innerText');
+//foodTittle.innerText = "food i can"
+
+//2
+
+
+//const brandElement = document.getElementById('brand');
+
+//brandElement.innerText = 'jalal slepping brand';
