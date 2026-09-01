5c71867c2a2f61437b27988920c42dc27bfeaad3
nazmulo <itsnaazmul@gmail.com>
2026-09-01 11:43:31 +0600

    addEventListener

--- events.js
+++ events.js
@@ -5,3 +5,28 @@ function makeYellow() {
 function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
+
+const btnMakeBlue = document.getElementById ('btn-make-blue');
+btnMakeBlue.onclick = function makeBlue () {
+    document.body.style.backgroundColor = "blue";
+}
+
+const btnMakePurple = document.getElementById ('btn-make-purple');
+btnMakePurple.onclick = makePurple ;
+function makePurple () {
+    document.body.style.backgroundColor = "purple";
+}
+
+//addEventListener :
+document.getElementById('btn-make-green').addEventListener('click',
+    function makeGreen() {
+        document.body.style.backgroundColor = 'green';
+    }
+)
+
+
+document.getElementById('btn-make-pink').addEventListener('click',
+    function () {
+        document.body.style.backgroundColor = 'pink';
+    }
+)
