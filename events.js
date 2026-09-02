29781af6a2a2532bf29eac4dec657ca0d9c3968d
nazmulo <itsnaazmul@gmail.com>
2026-09-02 23:09:29 +0600

    clean the text area

--- events.js
+++ events.js
@@ -53,4 +53,13 @@ document.getElementById('btn-post-comment').addEventListener('click', function()
         const newComment = commentTextBox.value;
               // step-3 : get the parent node where to publish comment
         const commentContainer = document.getElementById('comment-container');
+              // step -4 : create a comment p and set the comment text
+        const commentElement = document.createElement('p');
+        commentElement.classList.add('comment');
+        commentElement.innerText = "new comment"
+               //step -5 : append the new p tag to the parent node 
+        commentContainer.appendChild('commentElement');
+             //step -6 : clean the text area
+        commentTextBox.value = " ";
+
 })
