8fd2d6d7922656cf3c9babf06001270cbd858ebe
nazmulo <itsnaazmul@gmail.com>
2026-09-03 00:20:49 +0600

    keydown

--- events.js
+++ events.js
@@ -87,6 +87,10 @@ document.getElementById('btn-post-comment').addEventListener('click', function (
 //    console.log('toggle')
 //})
 
-document.getElementById('btn-mouse').addEventListener('keydown', function(){
-    console.log('toggle')
-})
+//document.getElementById('btn-mouse').addEventListener('keydown', function(){
+//    console.log('toggle')
+//})
+
+//document.getElementById('btn-mouse').addEventListener('keyup', function(){
+//    console.log('toggle')
+//})
